70b86a0c843b667dff24f896658ca6e1ebbaa39b
Travis Fischer <fisch0920@gmail.com>
2022-11-06 13:45:06 -0600

    🍽

--- src/get-movie.ts
+++ src/get-movie.ts
@@ -7,9 +7,9 @@ import { prisma } from './lib/db'
  * Computes the unique genres and keywords across all movies in the database.
  */
 async function main() {
-  // const imdbId = 'tt0405094' // lives of others
+  const imdbId = 'tt0405094' // lives of others
   // const imdbId = 'tt1016150'
-  const imdbId = 'tt0111161' // movie (shawshank redemption)
+  // const imdbId = 'tt0111161' // movie (shawshank redemption)
 
   const result = await prisma.movie.findUnique({
     where: {
@@ -17,7 +17,7 @@ async function main() {
     }
   })
 
-  // console.log(JSON.stringify(result, null, 2))
+  console.log(JSON.stringify(result, null, 2))
 
   // const tmdb = new TMDB({ bearerToken: process.env.TMDB_BEARER_TOKEN })
   // const credits = await tmdb.getMovieCredits(result.tmdbId)
